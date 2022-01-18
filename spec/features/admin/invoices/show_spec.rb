b741117801fc27fcf3af0452c40d3e822f8968d8
Mallory Vining <mallorygallery12@gmail.com>
2022-01-18 11:11:52 -0700

    Last features user story

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -59,4 +59,30 @@ RSpec.describe 'Admin Invoice Show Page' do
       expect(current_path).to eq(admin_invoices_path(:id))
       expect(@invoice_1.status).to eq('completed')
   end
+
+  describe 'bulk_discounts' do
+    it "displays total revenue and discounted revenue" do
+      merchant = Merchant.create!(name: 'Shop Here')
+      not_included_merchant = Merchant.create!(name: 'Dont Shop Here')
+      customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
+      item_1 = Item.create!(merchant_id: merchant.id, name: 'widget-1', description: 'widget description',
+                            unit_price: 100)
+      item_2 = Item.create!(merchant_id: merchant.id, name: 'widget-2', description: 'widget description',
+                            unit_price: 200)
+      invoice = Invoice.create!(customer_id: customer.id, status: 'completed')
+      invoice_item_1 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_1.id, quantity: 5,
+                                           unit_price: 3000)
+      invoice_item_2 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_2.id, quantity: 15,
+                                           unit_price: 2000)
+      discount_a = merchant.bulk_discounts.create({name:"Discount A", percentage: 20, threshold: 10 })
+      discount_b = merchant.bulk_discounts.create({name:"Discount B", percentage: 30, threshold: 15 })
+
+      visit admin_invoice_path(invoice.id)
+
+      within ".discounted_revenue" do
+        expect(page).to have_content("Total Revenue: $450.00")
+        expect(page).to have_content("Discounted Revenue: $360.00")
+      end
+    end
+  end
 end
